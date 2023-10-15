35b87ae2bcc897e9b76cd9266aaf821765a5441f
Churka25cm <146408174+Churka25cm@users.noreply.github.com>
2023-10-15 20:28:08 +0500

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -6,10 +6,11 @@ lines_per_page = 50  # Число строк на странице
 characters_per_line = 25  # Количество символов в строке
 symbol = 4  # Для хранения кода одного символа нужно 4 байта.
 
-page_in_Mb = round((characters_per_line * lines_per_page * 4) / 1024 * 1024, 4)
+page_in_Mb = round((characters_per_line * lines_per_page * 4) / (1024 * 1024), 4)
 # строки умножаем на кол-во символов и умножаем на размер 1 символа в байтах
 
 book_in_Mb = page_in_Mb * pages_in_the_book
+
 volume_book = information_volume / book_in_Mb
 volume_book = int(volume_book)
 
