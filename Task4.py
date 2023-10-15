6c859ec30d7437480c2d56d45b3263e86e0ec1aa
Churka25cm <146408174+Churka25cm@users.noreply.github.com>
2023-10-15 20:24:02 +0500

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,13 +1,9 @@
 users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
 
 # TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
-countUsers = len(users)
-uniqueVisits = len(set(users))
-usersDict = {
-    "Общее количество":0,
-    "Уникальные посещения":0
-}
-usersDict["Общее количество"] = countUsers
-usersDict["Уникальные посещения"] = uniqueVisits
+count_Users = len(users)
+unique_Visits = len(set(users))
 
-print(usersDict)
+users_Dict = {"Общее количество": count_Users, "Уникальные посещения": unique_Visits}
+
+print(users_Dict)
